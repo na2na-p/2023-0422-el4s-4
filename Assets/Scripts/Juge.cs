1c582904cd64de2353706b3006a7272f6345364d
herusapento <herusapento@outlook.jp>
2023-04-22 14:33:34 +0900

    判定処理コミット用

--- Assets/Scripts/Juge.cs
+++ Assets/Scripts/Juge.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WINNER
+{
+	PYALER_1,
+	PYALER_2,
+	DRAW
+}
+
 public class Juge : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -15,18 +22,20 @@ public class Juge : MonoBehaviour
     {
         
     }
-	/*
+	
 	struct Float2
 	{
 		public float x;
 		public float y; 
   };
+	
 	short Judgement(Float2 PlA, Float2 PlB, Float2 CenP)
 	{
 		float PlALen = (PlA.x - CenP.x) * (PlA.x - CenP.x) + (PlA.y - CenP.y) * (PlA.y - CenP.y);
 		float PlBLen = (PlB.x - CenP.x) * (PlB.x - CenP.x) + (PlB.y - CenP.y) * (PlB.y - CenP.y);
 		
 
+		GameObject.Find
 		if (PlALen > PlBLen)
 		{
 			return 1;
@@ -41,6 +50,5 @@ public class Juge : MonoBehaviour
 		}
 	}
 	//END of Judge
-	*/
 
 }
