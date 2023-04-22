a8f950ca1c6c2b1c8b5908a7ca06f1e9b7a69e4f
yuta6686 <yuta20010614@gmail.com>
2023-04-22 14:42:02 +0900

    タイマー作成

--- Assets/Script/CountDown.cs
+++ Assets/Script/CountDown.cs
@@ -8,18 +8,21 @@ public class CountDown : MonoBehaviour
 {
 	public static float CountDownTime;
 	public TextMeshProUGUI TextCountDown;
+	[SerializeField] float CountDownTimeMax = 60;
+	private bool IsFinish;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
     {
-			CountDownTime = 60.0f;
+			CountDownTime = CountDownTimeMax;
+			IsFinish = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 			// カウントダウンタイムを整形して表示
-			TextCountDown.text = string.Format("Time: {0:00.00}", CountDownTime);
+			TextCountDown.text = string.Format("{0:00.00}", CountDownTime);
 
 			// 経過時刻を弾いていく
 			CountDownTime -= Time.deltaTime;
@@ -27,6 +30,9 @@ public class CountDown : MonoBehaviour
 			if(CountDownTime <= 0.0F)
 			{
 				CountDownTime = 0.0F;
+				IsFinish = true;
 			}
     }
+
+	bool GetIsFinish() { return IsFinish; }
 }
